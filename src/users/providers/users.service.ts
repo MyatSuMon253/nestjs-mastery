2b314e4dc87ee867e1f3a31663afbc9d96de80af
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-05-02 15:55:25 +0630

    docs: setup compodoc and write documentation for full coverage

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -3,13 +3,26 @@ import { CreateUserDto } from '../dtos/create.user.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
 import { PutUserDto } from '../dtos/put.user.dto';
 
+/**
+ * Class to connect with Users table and make business operations
+ */
 @Injectable()
 export class UsersService {
+  /**
+   * Constructor to connect with other services to make operations
+   * @param authService
+   */
   constructor(
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
 
+  /**
+   * Method to get all users from the db
+   * @param limit
+   * @param page
+   * @returns
+   */
   public findAll(limit: number, page: number) {
     const isAuth = this.authService.isAuth();
 
@@ -23,10 +36,20 @@ export class UsersService {
     ];
   }
 
+  /**
+   * Method to create a new user
+   * @param createUserDto
+   * @returns
+   */
   public createUser(createUserDto: CreateUserDto) {
     return `${createUserDto.firstName}'s user account is created`;
   }
 
+  /**
+   * Method to find the specific user with id
+   * @param createUserDto
+   * @returns
+   */
   public findByUserId(userId: string) {
     return {
       id: userId,
@@ -35,6 +58,11 @@ export class UsersService {
     };
   }
 
+  /**
+   * Method to update existing user with given id
+   * @param putUserDto
+   * @returns
+   */
   public updateUser(putUserDto: PutUserDto) {
     return putUserDto;
   }
