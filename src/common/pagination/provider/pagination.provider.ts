a0d2ce376f6d39dd5781b9e50cfcba318eb96c78
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-06-16 09:19:59 +0630

    feat: return metadata

--- src/common/pagination/provider/pagination.provider.ts
+++ src/common/pagination/provider/pagination.provider.ts
@@ -1,14 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PaginationQueryDto } from '../dtos/pagination-query.dto';
 import { ObjectLiteral, Repository } from 'typeorm';
+import { REQUEST } from '@nestjs/core';
+import type { Request } from 'express';
+import { Paginated } from '../interfaces/paginated.interface';
 
 @Injectable()
 export class PaginationProvider {
+  constructor(
+    /** injecting request */
+    @Inject(REQUEST)
+    private readonly request: Request,
+  ) {}
+
   public async paginateQuery<T extends ObjectLiteral>(
     paginationQuery: PaginationQueryDto,
     repository: Repository<T>,
-  ) {
-    const result = await repository.find({
+  ): Promise<Paginated<T>> {
+    if (!paginationQuery.page || !paginationQuery.limit) {
+      throw new Error('Pagination query need limit and page.');
+    }
+
+    const results = await repository.find({
       skip:
         paginationQuery?.page && paginationQuery.limit
           ? (paginationQuery.page - 1) * paginationQuery.limit
@@ -16,6 +29,38 @@ export class PaginationProvider {
       take: paginationQuery.limit,
     });
 
-    return result;
+    const baseUrl =
+      this.request.protocol + '://' + this.request.headers.host + '/';
+    const newUrl = new URL(this.request.url, baseUrl);
+
+    const totalItems = await repository.count();
+    const totalPages =
+      paginationQuery?.limit && Math.ceil(totalItems / paginationQuery.limit);
+
+    const nextPage =
+      paginationQuery.page === totalPages
+        ? paginationQuery.page
+        : paginationQuery.page + 1;
+    const previousPage =
+      paginationQuery.page === 1
+        ? paginationQuery.page
+        : paginationQuery.page - 1;
+
+    return {
+      data: results,
+      metadata: {
+        itemsPerPage: paginationQuery.limit,
+        totalItems,
+        currentPage: paginationQuery.page,
+        totalPages,
+      },
+      links: {
+        first: `${newUrl.origin}${newUrl.pathname}?limit=${paginationQuery.limit}&page=1`,
+        last: `${newUrl.origin}${newUrl.pathname}?limit=${paginationQuery.limit}&page=${totalPages}`,
+        current: `${newUrl.origin}${newUrl.pathname}?limit=${paginationQuery.limit}&page=${paginationQuery.page}`,
+        next: `${newUrl.origin}${newUrl.pathname}?limit=${paginationQuery.limit}&page=${nextPage}`,
+        previous: `${newUrl.origin}${newUrl.pathname}?limit=${paginationQuery.limit}&page=${previousPage}`,
+      },
+    };
   }
 }
